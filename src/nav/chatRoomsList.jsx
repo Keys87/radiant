61fb74f1428d0f5f5d1d57c27b3598d118cfddc8
Keys87-Desk <119033902+Keys87@users.noreply.github.com>
2025-03-23 15:11:19 +0700

    modify vite config and removed 2 new lines on chatRoomsList

--- src/nav/chatRoomsList.jsx
+++ src/nav/chatRoomsList.jsx
@@ -1,6 +1,4 @@
 export function ChatRoomsList({show}) {
-    
-
     return (
         <section role="" className={`bg-black/40 w-52 flex-initial flex-col justify-center align-top border-e-2 ${show}`}>
             <h3 className="font-bold text-2xl text-white ms-2 mt-2 text-center">Chats</h3>
